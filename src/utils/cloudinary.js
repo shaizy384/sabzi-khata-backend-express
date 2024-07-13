edc7ac0c27f4318e7cb79d9c179e3334ab1c9458
Shahzaib Qasim <122147898+shaizy384@users.noreply.github.com>
2024-07-12 22:29:32 -0700

    update cloudinary config

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,15 +1,18 @@
 import { v2 as cloudinary } from 'cloudinary';
 import fs from "fs"
+import dotenv from "dotenv"
+
+dotenv.config()
 
 cloudinary.config({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-    api_key: process.env.CLOUDINARY_API_KEY,
+    api_key: process.env.CLOUDINARY_APIKEY,
     api_secret: process.env.CLOUDINARY_API_SECRET
 });
 
 const uploadOnCloudinary = async (localFilepath) => {
     try {
-        console.log("cloudinary config: ", process.env.CLOUDINARY_CLOUD_NAME, process.env.CLOUDINARY_API_KEY, process.env.CLOUDINARY_API_SECRET);
+        console.log("cloudinary config: ", process.env.CLOUDINARY_CLOUD_NAME, process.env.CLOUDINARY_APIKEY, process.env.CLOUDINARY_API_SECRET);
         console.log("localFilepath ", localFilepath);
         const response = await cloudinary.uploader.upload(localFilepath, {
             resource_type: "auto"
